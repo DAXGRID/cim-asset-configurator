7309efd1eabaeea041223e74b1f7c4d2c5970b4b
Rune Nielsen <runeanielsen@live.dk>
2020-05-04 15:54:17 +0200

    adds prettierrc and formatted

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -2,43 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const getNamespace = (x) => {
-    if (x.id && x.entities && x.entities.length > 0)
-        return <li><ul>{entities(x)}</ul></li>;
+  if (x.id && x.entities && x.entities.length > 0)
+    return <li><ul>{entities(x)}</ul></li>;
 }
 
 const entities = (namespace) => {
-    return namespace.entities.map((x, index) => {
+  return namespace.entities.map((x, index) => {
 
-        let elements = null;
-        let currentEntity = x;
+    let elements = null;
+    let currentEntity = x;
 
-        while(currentEntity !== null) {
-            elements += entityElement(currentEntity);
-            currentEntity = null;
-        }
+    while (currentEntity !== null) {
+      elements += entityElement(currentEntity);
+      currentEntity = null;
+    }
 
-        return <ul key={index}>{elements}</ul>;
-    });
+    return <ul key={index}>{elements}</ul>;
+  });
 }
 
 const entityElement = (entity) => {
-    return <li title={entity.description} key={entity.id}>{entity.name}<ul>{attributes(entity)}</ul></li>;
+  return <li title={entity.description} key={entity.id}>{entity.name}<ul>{attributes(entity)}</ul></li>;
 }
 
 const attributes = (entity) => {
-    return entity.attributes.map((x, i) => {
-        return <li title={x.description} key={i}>{x.name}</li>;
-    });
+  return entity.attributes.map((x, i) => {
+    return <li title={x.description} key={i}>{x.name}</li>;
+  });
 }
 
-const MenuItem = ( { namespace } ) => {
-    return (
-        <div>{getNamespace(namespace)}</div>
-    );
+const MenuItem = ({ namespace }) => {
+  return (
+    <div>{getNamespace(namespace)}</div>
+  );
 }
 
 MenuItem.propTypes = {
-    namespace: PropTypes.object
+  namespace: PropTypes.object
 };
 
 export default MenuItem;
--- src/components/side-menu/side-menu.js
+++ src/components/side-menu/side-menu.js
@@ -4,47 +4,47 @@ import MenuItem from '../menu-item';
 const electron = window.require('electron');
 const fs = electron.remote.require('fs');
 
-const assetSchemaJson = fs.readFileSync('./../asset-schema.json', 'UTF8');
+const assetSchemaJson = fs.readFileSync('./asset-schema.json', 'UTF8');
 const schema = JSON.parse(assetSchemaJson);
 
 const getNamespaces = () => {
-    return schema.namespaces.map((namespace) => {
-        let clonedNamespace = JSON.parse(JSON.stringify(namespace));
+  return schema.namespaces.map((namespace) => {
+    let clonedNamespace = JSON.parse(JSON.stringify(namespace));
 
-        clonedNamespace.entities = namespace.entities.filter((entity) => {
-            return !(isPrimitiveType(entity));
-        });
-
-        if (!clonedNamespace.entities)
-            return null;
-        
-        return clonedNamespace;
+    clonedNamespace.entities = namespace.entities.filter((entity) => {
+      return !(isPrimitiveType(entity));
     });
+
+    if (!clonedNamespace.entities)
+      return null;
+
+    return clonedNamespace;
+  });
 }
 
 const isPrimitiveType = (entity) => {
-    if (
-        entity.stereoType === 'Compound' || 
-        entity.stereoType === 'CIMDatatype' ||
-        entity.stereoType === 'enumeration' ||
-        entity.stereoType === 'Primitive'
-    ) {
-        return true;
-    }
+  if (
+    entity.stereoType === 'Compound' ||
+    entity.stereoType === 'CIMDatatype' ||
+    entity.stereoType === 'enumeration' ||
+    entity.stereoType === 'Primitive'
+  ) {
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 const SideMenu = () => {
-    return (
-        <div className="side-menu">
-          <ul className="tree-view">
-            {getNamespaces().map((x, i) => {
-                return <MenuItem key={i} namespace={x} />;
-            })}
-          </ul>
-        </div>
-    );
+  return (
+    <div className="side-menu">
+      <ul className="tree-view">
+        {getNamespaces().map((x, i) => {
+          return <MenuItem key={i} namespace={x} />;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default SideMenu;
